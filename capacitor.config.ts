02adc75ed79fe338a6a4010002601a94706f1618
ani <amazinganirudhhere@gmail.com>
2024-04-30 12:11:15 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Author Identification',
+  webDir: 'dist'
+};
+
+export default config;
